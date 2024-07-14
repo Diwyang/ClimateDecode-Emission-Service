e29ec5fd4b039c154d16cad8923882529b235fae
Diwyang Jain <Diwyang.Jain@teliolabs.com>
2024-07-14 23:40:33 +0530

    files committed

--- src/main/java/com/climate/decode/event/emissions/EventEmissionServiceApplication.java
+++ src/main/java/com/climate/decode/event/emissions/EventEmissionServiceApplication.java
@@ -2,8 +2,10 @@ package com.climate.decode.event.emissions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventEmissionServiceApplication {
 
 	public static void main(String[] args) {
